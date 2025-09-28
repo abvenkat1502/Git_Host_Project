bdcfb6429f974e1468e46ea134cecd9366a3cf22
ELCOT <ELCOT@Lenovo>
2025-09-28 20:45:28 +0530

    Initial Commit with entire project

---
+++ Git_Host_Project/src/com/sample/files/TextClass.java
@@ -0,0 +1,12 @@
+package com.sample.files;
+
+public class TextClass {
+	
+	//SAMPLE CLASS OR HOSTING PROJECT TO THE GIT
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
