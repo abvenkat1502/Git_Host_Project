23c9777fd2f8e0aeccdbb9d2e250ad39ce7f3220
ELCOT <ELCOT@Lenovo>
2025-09-28 20:52:39 +0530

    Second Commit

--- Git_Host_Project/src/com/sample/files/TextClass.java
+++ Git_Host_Project/src/com/sample/files/TextClass.java
@@ -3,6 +3,7 @@ package com.sample.files;
 public class TextClass {
 	
 	//SAMPLE CLASS OR HOSTING PROJECT TO THE GIT
+	//ADDED SOMETHING
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
